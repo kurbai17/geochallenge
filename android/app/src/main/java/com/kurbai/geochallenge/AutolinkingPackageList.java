b90b5ce00d41ed5b0a9fe8a4f82399f8a41c0d12
Anna Corberó <anna@MacBook-Pro-de-Anna.local>
2025-05-12 19:10:57 +0200

    Agrega AutolinkingPackageList manual y desactiva generación automática

---
+++ android/app/src/main/java/com/kurbai/geochallenge/AutolinkingPackageList.java
@@ -0,0 +1,12 @@
+package com.kurbai.geochallenge;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.PackageList;
+
+import java.util.List;
+
+public class AutolinkingPackageList {
+    public static List<ReactPackage> get() {
+        return new PackageList(null).getPackages();
+    }
+}
