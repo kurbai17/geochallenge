4177722e0b1f2754790378e52c7d12c18d3076ba
Anna Corberó <anna@MacBook-Pro-de-Anna.local>
2025-05-12 19:30:41 +0200

    Define paquetes manualmente sin usar PackageList

--- android/app/src/main/java/com/kurbai/geochallenge/AutolinkingPackageList.java
+++ android/app/src/main/java/com/kurbai/geochallenge/AutolinkingPackageList.java
@@ -1,12 +1,31 @@
 package com.kurbai.geochallenge;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.PackageList;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.rnappauth.RNAppAuthPackage;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class AutolinkingPackageList {
     public static List<ReactPackage> get() {
-        return new PackageList(null).getPackages();
+        return Arrays.asList(
+            new RNCWebViewPackage(),
+            new RNGestureHandlerPackage(),
+            new ReanimatedPackage(),
+            new RNScreensPackage(),
+            new LinearGradientPackage(),
+            new SplashScreenReactPackage(),
+            new RNAppAuthPackage(),
+            new SafeAreaContextPackage(),
+            new MapsPackage()
+        );
     }
 }
